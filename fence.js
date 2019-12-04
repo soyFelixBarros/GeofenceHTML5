4ba5f0fe79fe273ac57d8a0fc237b1d4a9c5d5df
Felix Barros <soyfelixbarros@gmail.com>
2019-12-04 10:03:54 -0300

    ahora se puede agrega multiples puntos de inicio

--- fence.js
+++ fence.js
@@ -1,56 +1,63 @@
-   window.onload = function() {
-        var startPos;
-        var startPosLat;
-        var startPosLong;
-        var distance;
+window.onload = function() {
+  var startPos;
+  var startPosLat;
+  var startPosLong;
+  var distance;
       
-        if (navigator.geolocation) {
+  if (navigator.geolocation) {
+    //latitude, longitude
+    var coordinates = [
+      [-27.4552413,-58.9873352],
+      [-27.4563524,-58.9873445],
+      [-27.4564516,-58.9995527]
+    ];
 
-          startPosLat = 44.95716993150707;
-          startPosLong = -93.28439280496818;
-
-          $("#startLat").text(startPosLat);
-          $("#startLon").text(startPosLong);
+    navigator.geolocation.watchPosition(function(position) {
+      $("#currentLat").text(position.coords.latitude);
+      $("#currentLon").text(position.coords.longitude);
       
-          navigator.geolocation.watchPosition(function(position) {
-            $("#currentLat").text(position.coords.latitude);
-            $("#currentLon").text(position.coords.longitude);
+      coordinates.forEach(function(item, index, array) {
+        distance = calculateDistance(item[0], item[1], position.coords.latitude, position.coords.longitude);
+        let ul = $("#startLatLon");
+        let className ='list-'+index;
+        ul.find('li.'+className).remove(); // Eliminamos el li si existe
+        ul.append('<li class="'+className+'">'+item[0]+', '+item[1]+'<br>Distancia: '+distance+' mts -> '+getMessage(distance)+'</li>'); // Agregamos el nuevo
+      });
+    });
+  }
+};
 
-            distance = calculateDistance(startPosLat, startPosLong,position.coords.latitude, position.coords.longitude)
-            $("#distance").text(distance);
+function getMessage(distance) {
+  let message = '';
+  if (distance > .00 && distance < .05) {
+    message = 'está dentro de .00 y .05';
+  }
+  else if (distance > .05 && distance < .10) {
+    message = 'está dentro de .05 y .10';
+  }
+  else if (distance > .10 && distance < .15) {
+    message = 'está dentro de .10 y .15';
+  }
+  else if (distance > .15) {
+    message = 'está muy lejos';
+  }
+  return message;
+}
+// Reused code - copyright Moveable Type Scripts - retrieved May 4, 2010.
+// http://www.movable-type.co.uk/scripts/latlong.html
+// Under Creative Commons License http://creativecommons.org/licenses/by/3.0/
+function calculateDistance(lat1, lon1, lat2, lon2) {
+  var R = 6371; // km
+  var dLat = (lat2-lat1).toRad();
+  var dLon = (lon2-lon1).toRad();
+  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+          Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) *
+          Math.sin(dLon/2) * Math.sin(dLon/2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  var d = R * c;
+  return d;
+}
 
-            let message = $("#message");
-
-            if (distance > .00 && distance < .05) {
-              message.text('está dentro de .00 y .05');
-            }
-            else if(distance > .05 && distance < .10) {
-              message.text('está dentro de .05 y .10');
-            }
-            else if(distance > .10 && distance < .15) {
-              message.text('está dentro de .10 y .15');
-            }
-            else if(distance > .15) {
-              message.text('está muy lejos');
-            }
-          });
-        }
-      };
-      
-      // Reused code - copyright Moveable Type Scripts - retrieved May 4, 2010.
-      // http://www.movable-type.co.uk/scripts/latlong.html
-      // Under Creative Commons License http://creativecommons.org/licenses/by/3.0/
-      function calculateDistance(lat1, lon1, lat2, lon2) {
-        var R = 6371; // km
-        var dLat = (lat2-lat1).toRad();
-        var dLon = (lon2-lon1).toRad();
-        var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-                Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) *
-                Math.sin(dLon/2) * Math.sin(dLon/2);
-        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-        var d = R * c;
-        return d;
-      }
-      Number.prototype.toRad = function() {
-        return this * Math.PI / 180;
-      }
+Number.prototype.toRad = function() {
+  return this * Math.PI / 180;
+}
